448bfc9fc0525a4769418ccdca40963e3ecc389f
Eduardo Silva <eduardo@open-ecommerce.org>
2025-04-08 17:11:51 +0100

    bootstrap5

--- library/bootstrap/ActiveForm.php
+++ library/bootstrap/ActiveForm.php
@@ -11,7 +11,7 @@ use usni\library\bootstrap\ActiveField;
  * 
  * @package usni\library\bootstrap
  */
-class ActiveForm extends \yii\bootstraps5\ActiveForm
+class ActiveForm extends \yii\bootstrap5\ActiveForm
 {
     /**
      * @inheritdoc
